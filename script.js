c80e281818ebf255d0034897b250350a3a2e1c16
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-01-22 19:02:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// console.log(fruit [0])
+// console.log(fruit[1] = 'watermelon')
+// console.log(fruit)        //O'zgartitish
+// console.log(fruit[2] ='avacado')
+// console.log(fruit)        //Qo'shish
+// console.log(fruit.at(-1)) //Eng oxirgi sozni topish
+
+// let fruit = ['applle' ,'lemon' ];
+// console.log(fruit.pop())
+// console.log(fruit)        //Eng oxiridan ochiradi
+
+// console.log(fruit.push('watermelon'))
+// console.log(fruit)        //Eng oxiridan qoshadi
+
+// console.log(fruit.unshift('watermelon'))
+// console.log(fruit)        //Eng boshidan qoshish
+
+// console.log(fruit.shift())
+// console.log(fruit)           //Eng boshidan o'chirish
+
+let fruit = ['ismoil','Aziza','Madina','Lola'];
+console.log(fruit)
+let fruit1 = alert('Boshlanishida array uzunligi 4');
+let first = confirm('Siz arrayni malumotlarini olib tashlamoiqchimisiz')
+if(first === true){
+    let fruit2 = alert('arrayning uzunligi 3');
+    
+       console.log(fruit.pop());
+       console.log(fruit)
+
+    
+}
+else{
+    let fruit3 = alert('arrayning uzunligi 4');
+    console.log(fruit)
+
+}
